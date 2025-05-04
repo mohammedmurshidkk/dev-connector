51368f2cb07f69191f467878acdbfb2062ac05ee
Mohammed Murshid KK <muhammedmurshid43@gmail.com>
2025-05-04 23:56:48 +0530

    Verification tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import '../styles/variables.css';
 import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
+        <meta name='google-site-verification' content='googleb38bae8cad4a7346.html' />
         <script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8910149152066720'
@@ -30,6 +32,7 @@ export default function RootLayout({ children }) {
         ></script>
       </head>
       <body className={inter.className}>
+        <Analytics />
         <div className='min-h-screen flex flex-col'>
           <Navigation />
           <main className='flex-grow flex flex-col'>{children}</main>
